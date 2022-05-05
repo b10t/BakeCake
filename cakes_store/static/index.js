34767f3b558f63ee7ff0f6902f86a44335872b02
b10t <b10t.rus@gmail.com>
2022-05-05 08:48:11 +0800

    Returned the calculation of the cake.

--- cakes_store/static/index.js
+++ cakes_store/static/index.js
@@ -1,5 +1,6 @@
 Vue.createApp({
     name: "App",
+    delimiters: ["[[", "]]"],
     components: {
         VForm: VeeValidate.Form,
         VField: VeeValidate.Field,
@@ -12,64 +13,62 @@ Vue.createApp({
                     if (value) {
                         return true;
                     }
-                    return ' количество уровней';
+                    return " количество уровней";
                 },
                 form: (value) => {
                     if (value) {
                         return true;
                     }
-                    return ' форму торта';
+                    return " форму торта";
                 },
                 topping: (value) => {
                     if (value) {
                         return true;
                     }
-                    return ' топпинг';
-                }
+                    return " топпинг";
+                },
             },
             schema2: {
                 name: (value) => {
                     if (value) {
                         return true;
                     }
-                    return ' имя';
+                    return " имя";
                 },
                 phone: (value) => {
                     if (value) {
                         return true;
                     }
-                    return ' телефон';
+                    return " телефон";
                 },
                 name_format: (value) => {
-                    const regex = /^[a-zA-Zа-яА-Я]+$/
+                    const regex = /^[a-zA-Zа-яА-Я]+$/;
                     if (!value) {
                         return true;
                     }
-                    if ( !regex.test(value)) {
-
-                        return '⚠ Формат имени нарушен';
+                    if (!regex.test(value)) {
+                        return "⚠ Формат имени нарушен";
                     }
                     return true;
                 },
                 email_format: (value) => {
-                    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+                    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
                     if (!value) {
                         return true;
                     }
-                    if ( !regex.test(value)) {
-
-                        return '⚠ Формат почты нарушен';
+                    if (!regex.test(value)) {
+                        return "⚠ Формат почты нарушен";
                     }
                     return true;
                 },
-                phone_format:(value) => {
-                    const regex = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
+                phone_format: (value) => {
+                    const regex =
+                        /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
                     if (!value) {
                         return true;
                     }
-                    if ( !regex.test(value)) {
-
-                        return '⚠ Формат телефона нарушен';
+                    if (!regex.test(value)) {
+                        return "⚠ Формат телефона нарушен";
                     }
                     return true;
                 },
@@ -77,33 +76,50 @@ Vue.createApp({
                     if (value) {
                         return true;
                     }
-                    return ' почту';
+                    return " почту";
                 },
                 address: (value) => {
                     if (value) {
                         return true;
                     }
-                    return ' адрес';
+                    return " адрес";
                 },
                 date: (value) => {
                     if (value) {
                         return true;
                     }
-                    return ' дату доставки';
+                    return " дату доставки";
                 },
                 time: (value) => {
                     if (value) {
                         return true;
                     }
-                    return ' время доставки';
-                }
+                    return " время доставки";
+                },
             },
             DATA: {
-                Levels: ['не выбрано', '1', '2', '3'],
-                Forms: ['не выбрано', 'Круг', 'Квадрат', 'Прямоугольник'],
-                Toppings: ['не выбрано', 'Без', 'Белый соус', 'Карамельный', 'Кленовый', 'Черничный', 'Молочный шоколад', 'Клубничный'],
-                Berries: ['нет', 'Ежевика', 'Малина', 'Голубика', 'Клубника'],
-                Decors: [ 'нет', 'Фисташки', 'Безе', 'Фундук', 'Пекан', 'Маршмеллоу', 'Марципан']
+                Levels: ["не выбрано", "1", "2", "3"],
+                Forms: ["не выбрано", "Круг", "Квадрат", "Прямоугольник"],
+                Toppings: [
+                    "не выбрано",
+                    "Без",
+                    "Белый соус",
+                    "Карамельный",
+                    "Кленовый",
+                    "Черничный",
+                    "Молочный шоколад",
+                    "Клубничный",
+                ],
+                Berries: ["нет", "Ежевика", "Малина", "Голубика", "Клубника"],
+                Decors: [
+                    "нет",
+                    "Фисташки",
+                    "Безе",
+                    "Фундук",
+                    "Пекан",
+                    "Маршмеллоу",
+                    "Марципан",
+                ],
             },
             Costs: {
                 Levels: [0, 400, 750, 1100],
@@ -111,38 +127,43 @@ Vue.createApp({
                 Toppings: [0, 0, 200, 180, 200, 300, 350, 200],
                 Berries: [0, 400, 300, 450, 500],
                 Decors: [0, 300, 400, 350, 300, 200, 280],
-                Words: 500
+                Words: 500,
             },
             Levels: 0,
             Form: 0,
             Topping: 0,
             Berries: 0,
             Decor: 0,
-            Words: '',
-            Comments: '',
+            Words: "",
+            Comments: "",
             Designed: false,
 
-            Name: '',
+            Name: "",
             Phone: null,
             Email: null,
             Address: null,
             Dates: null,
             Time: null,
-            DelivComments: ''
-        }
+            DelivComments: "",
+        };
     },
     methods: {
         ToStep4() {
-            this.Designed = true
+            this.Designed = true;
             setTimeout(() => this.$refs.ToStep4.click(), 0);
-        }
+        },
     },
     computed: {
         Cost() {
-            let W = this.Words ? this.Costs.Words : 0
-            return this.Costs.Levels[this.Levels] + this.Costs.Forms[this.Form] +
-                this.Costs.Toppings[this.Topping] + this.Costs.Berries[this.Berries] +
-                this.Costs.Decors[this.Decor] + W
-        }
-    }
-}).mount('#VueApp')
+            let W = this.Words ? this.Costs.Words : 0;
+            return (
+                this.Costs.Levels[this.Levels] +
+                this.Costs.Forms[this.Form] +
+                this.Costs.Toppings[this.Topping] +
+                this.Costs.Berries[this.Berries] +
+                this.Costs.Decors[this.Decor] +
+                W
+            );
+        },
+    },
+}).mount("#VueApp");
