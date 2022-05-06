357255b92b6661f3e061cfe79f89103409c34d09
b10t <b10t.rus@gmail.com>
2022-05-06 08:37:23 +0800

    Add reg function.

--- bake_cake/urls.py
+++ bake_cake/urls.py
@@ -7,5 +7,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('reg/', views.reg, name='index'),
     path('', views.index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cakes_store/views.py
+++ cakes_store/views.py
@@ -6,3 +6,24 @@ def index(request):
         'Step': 'Number',
     }
     return render(request, 'index.html', context)
+
+
+def reg(request):
+    context = {
+        'Step': 'Number',
+    }
+
+    if request.method == 'POST':
+        return render(request, 'index.html', context)
+        # form = RegisterUserForm_fiz(request.POST)
+        # if form.is_valid():
+        #     contract = form.save()
+        #     return render(request, 'registration/register_ok.html')
+        # else:
+        #     return render(request, 'registration/register_fiz.html', {'form': form})
+    else:
+        return render(request, 'index.html', context)
+
+        # form = RegisterUserForm_fiz()
+        # return render(request, 'registration/register_fiz.html', {'form': form})
+    # return render(request, 'main/uconstruction.html')
