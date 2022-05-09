a5de739d7bce4fc9f85d97def2af2533bece19b3
CaDiBob <nikolay.v.byankin@gmail.com>
2022-05-09 14:23:44 +0900

    Настроил статику

--- bake_cake/settings.py
+++ bake_cake/settings.py
@@ -130,7 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = os.getenv('STATIC_URL', '/static/')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'cakes_store/static'), )
+
+STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
